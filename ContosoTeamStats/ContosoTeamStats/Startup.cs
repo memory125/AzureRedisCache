4d0995033608a59ed4e988228ca4b6a39954989f
memory125 <mengde1120@sina.com>
2016-11-23 12:28:12 +0800

    Create Redis Cache Demo

---
+++ ContosoTeamStats/ContosoTeamStats/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContosoTeamStats.Startup))]
+namespace ContosoTeamStats
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
